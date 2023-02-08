abfa2b15a249641406c747f5d9bb7fbfe2d5c5a1
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-08 11:23:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+function RelativeList() {
+    const relatives = [
+        { id: 1, name: 'Uncle Bharat' },
+        { id: 2, name: 'Aunt Saloni' },
+        { id: 3, name: 'Cousin Krish' }
+    ];
+
+    return (
+        <ol>
+            {relatives.map(relative => (
+                <li key={relative.id}>{relative.name}</li>
+            ))}
+        </ol>
+    );
+}
+
+export default RelativeList;
